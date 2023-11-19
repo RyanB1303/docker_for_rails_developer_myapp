878b5fb6a24dc0c4f9e3e28a3b4335d64fd7e2ef
ryanb1303 <ryan.brilliant.nirwana@gmail.com>
2023-11-19 10:55:54 +0700

    add headless browser for faster test

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,25 @@
+require 'selenium/webdriver'
 # Add options for the Chrome browser
 chrome_options = Selenium::WebDriver::Chrome::Options.new
 
 # Disable notifications
 chrome_options.add_argument("--disable-notifications")
 
+headless_chrome_options = Selenium::WebDriver::Chrome::Options.new
+headless_chrome_options.add_argument("headless")
+headless_chrome_options.add_argument("disable-gpu")
+
+
 Capybara.register_driver :selenium_chrome_in_container do |app|
   Capybara::Selenium::Driver.new app,
     browser: :remote,
     url: "http://selenium_chrome:4444/wd/hub",
     options: chrome_options
+end
+
+Capybara.register_driver :headless_selenium_chrome_in_container do |app|
+  Capybara::Selenium::Driver.new app,
+    browser: :remote,
+    url: "http://selenium_chrome:4444/wd/hub",
+    options: headless_chrome_options
 end
